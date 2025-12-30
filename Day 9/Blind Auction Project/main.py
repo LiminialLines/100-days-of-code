0383e55f485eb20057165da05d14fcb04b725ea1
rrutt <richiesocialmedia95@gmail.com>
2025-12-29 18:01:04 -0600

    working on day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -5,3 +5,19 @@
 
 import art
 print(art.logo)
+def auctioneer():
+    bidding = True
+    partners = {}
+    while bidding:
+        name = input("Howdy partner! What's your rootin tootin daggone name?\n")
+        bid = int(input("And how many of the good government's dollars are you bidding today?\n"))
+        partners[name] = bid
+        more = input("Any other bidders? yes/no\n").lower()
+        if more == "no":
+            bidding = False
+    return partners
+bids = auctioneer()
+winner = max(bids, key=bids.get)
+highest_bid = bids[winner]
+
+print(f"Winner: {winner} with ${highest_bid}")
